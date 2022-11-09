9d99e853b878b9eab8b57f6a8af0d4f4f9ac3658
Abdur Nur Tushar Irfan <tusharctg1202@gmail.com>
2022-11-09 13:33:27 +0600

    new things added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -18,18 +19,39 @@ app.get('/', (req, res) => {
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fpvwzmp.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unatuhorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 async function run() {
     try {
         const serviceCollection = client.db('photoDb').collection('services')
         const reviewsCollection = client.db('photoDb').collection('reviews')
+        //for jwt token
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1hr' })
+            res.send({ token })
+
+        })
 
         //for services
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollection.find(query)
-            const services = await cursor.limit(3).toArray();
+            const services = await cursor.sort({_id:-1}).limit(3).toArray();
             res.send(services)
         })
 
@@ -40,6 +62,15 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        //for add services
+
+        // post for add allServices
+        app.post("/addService", async (req, res) => {
+            const review = req.body;
+            const result = await serviceCollection.insertOne(review);
+            res.send(result);
+        });
         //for specific service
 
         app.get('/services/:id', async (req, res) => {
@@ -51,6 +82,7 @@ async function run() {
 
         //for post
         app.post('/reviews', async (req, res) => {
+
             const review = req.body
             const result = await reviewsCollection.insertOne(review)
             res.send(result)
@@ -67,20 +99,22 @@ async function run() {
         //for update
         app.patch('/reviews/:id', async (req, res) => {
             const id = req.params.id;
-            const status = req.params.status;
+            const updateUser = req.body;
             const query = { _id: ObjectId(id) }
+            const Option = { upsert: true }
             const updateDoc = {
                 $set: {
-                    status: status
+                    serviceName: updateUser.serviceName,
+                    date: updateUser.date,
+                    message: updateUser.message
 
                 }
             }
-            const result = await reviewsCollection.updateOne(query, updateDoc)
+            const result = await reviewsCollection.updateOne(query, updateDoc, Option)
             res.send(result)
 
         })
         app.get('/reviews', async (req, res) => {
-           
             let query = {}
             if (req.query.serviceName) {
                 query = {
@@ -90,14 +124,21 @@ async function run() {
 
             }
             const cursor = reviewsCollection.find(query)
-            const reviews = await cursor.toArray()
+            const reviews = await cursor.sort({date: 1}).toArray()
             res.send(reviews)
         })
 
 
         //for review by email
-        app.get('/reviews', async (req, res) => {
-            
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            console.log(req.headers)
+
+            const decoded = req.decoded;
+            console.log('Inside order api', decoded)
+            if (decoded?.email !== req.query.email) {
+                res.status(403).send({ message: 'Unauthorized access' })
+            }
+
             let query = {}
             if (req.query.email) {
                 query = {
